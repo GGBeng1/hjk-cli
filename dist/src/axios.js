0391f54e0634c791de76eb9e31ad139e9af91094
GGBeng1 <15010976823@163.com>
2019-09-12 10:26:53 +0800

    更新脚手架基础内容，将webpack移除，并全部更新至最新版本

--- dist/src/axios.js
+++ dist/src/axios.js
@@ -1,19 +1,6 @@
-import axios from "axios";
-import store from "./store";
-import { Loading } from "element-ui";
-import router from "./router.js";
-import qs from "qs";
-
-let $alert = function (message, title, options) {
-  window.bus.$alert(message, title, options);
-};
-let $msgbox = function (options) {
-  window.bus.$msgbox(options);
-};
-
-let cancel,
-  promiseArr = {};
-const CancelToken = axios.CancelToken;
+import axios from 'axios'
+import { Loading } from 'element-ui'
+// import qs from 'qs'
 
 /**
  * axios请求拦截器
@@ -22,27 +9,20 @@ const CancelToken = axios.CancelToken;
  **/
 axios.interceptors.request.use(
   config => {
-    let loading = Loading.service({
+    Loading.service({
       fullscreen: true,
       lock: true,
-      text: "拼命加载中..."
-    });
+      text: '拼命加载中...'
+    })
 
-    if (promiseArr[config.url]) {
-      promiseArr[config.url]("操作取消");
-      promiseArr[config.url] = cancel;
-    } else {
-      promiseArr[config.url] = cancel;
-    }
-
-    return config;
+    return config
   },
   error => {
-    let loading = Loading.service({});
-    loading.close();
-    return Promise.reject(error);
+    const loading = Loading.service({})
+    loading.close()
+    return Promise.reject(error)
   }
-);
+)
 
 /**
  * axios 响应拦截器
@@ -51,177 +31,15 @@ axios.interceptors.request.use(
  **/
 axios.interceptors.response.use(
   response => {
-    let loading = Loading.service({});
-    loading.close();
-
-    return response;
+    const loading = Loading.service({})
+    loading.close()
+    return response
   },
   error => {
-    let loading = Loading.service({});
-    loading.close();
-    return Promise.resolve(error);
+    const loading = Loading.service({})
+    loading.close()
+    return Promise.resolve(error)
   }
-);
+)
 
-let codeAction = {
-  401: () => {
-    $alert("当前已经退出登陆", "提示", {
-      confirmButtonText: "确定",
-      callback: () => {
-        router.push({ path: "/" });
-      }
-    });
-  },
-  402: ({ config: { url } }) => {
-    $alert("无访问权限，路径：" + url, "提示", {
-      confirmButtonText: "确定",
-      callback: () => { }
-    });
-  },
-  403: ({ config: { url } }) => {
-    $alert("无访问权限，路径：" + url, "提示", {
-      confirmButtonText: "确定",
-      callback: () => { }
-    });
-  },
-  404: ({ config: { url } }) => {
-    $alert("资源不存在，路径：" + url, "提示", {
-      confirmButtonText: "确定",
-      callback: () => { }
-    });
-  },
-  500: ({ data, config: { url } }) => {
-    const h = window.bus.$createElement;
-    $msgbox({
-      title: "提示",
-      message: h("div", null, [
-        h("span", null, data.message),
-        h("el-collapse", null, [
-          h("el-collapse-item", null, [h("div", null, data.data)])
-        ])
-      ]),
-      confirmButtonText: "确定"
-    });
-  }
-};
-//判断code
-function checkStatus (response) {
-  let {
-    data,
-    config: { url }
-  } = response;
-  if (data) {
-    let action = codeAction[data.code];
-    if (action) {
-      action(response);
-      return new Promise(() => { });
-    }
-  }
-  return data;
-}
-
-// axios.defaults.baseURL = '/hid'
-//设置默认请求头
-axios.defaults.headers = {
-  "X-Requested-With": "XMLHttpRequest",
-  "Content-Type": "application/json"
-};
-axios.defaults.timeout = 10000;
-
-export default {
-  get (url, param) {
-    return new Promise((resolve, reject) => {
-      if (url instanceof Object && url != null) {
-        axios.get(url).then(res => {
-          // checkStatus(res)
-          resolve(checkStatus(res));
-        });
-        return;
-      }
-      axios({
-        method: "get",
-        url,
-        params: param,
-        cancelToken: new CancelToken(c => {
-          cancel = c;
-        })
-      }).then(res => {
-        resolve(checkStatus(res));
-      });
-    });
-  },
-  //json格式
-  postJson (url, param, data) {
-    return new Promise((resolve, reject) => {
-      if (url instanceof Object && url != null) {
-        axios({
-          method: "post",
-          url: url.url,
-          params: url.param,
-          data: url.data,
-          cancelToken: new CancelToken(c => {
-            cancel = c;
-          })
-        }).then(res => {
-          //  checkStatus(checkStatus(res));
-          resolve(checkStatus(res));
-        });
-        return;
-      }
-      axios({
-        method: "post",
-        url,
-        params: param,
-        data,
-        cancelToken: new CancelToken(c => {
-          cancel = c;
-        })
-      }).then(res => {
-        resolve(checkStatus(res));
-        //    checkStatus(res);
-      });
-    });
-  },
-  post (url, data, param) {
-    let datas = qs.stringify(data);
-    return new Promise((resolve, reject) => {
-      if (url instanceof Object && url != null) {
-        axios({
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          method: "post",
-          url: url.url,
-          params: url.param,
-          data: url.data,
-          cancelToken: new CancelToken(c => {
-            cancel = c;
-          })
-        }).then(res => {
-          resolve(checkStatus(res));
-          //   checkStatus(res);
-        });
-        return;
-      }
-      axios({
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "post",
-        url,
-        params: param,
-        data: datas,
-        cancelToken: new CancelToken(c => {
-          cancel = c;
-        })
-      })
-        .then(res => {
-          resolve(checkStatus(res));
-          //checkStatus(res);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-};
+axios.defaults.timeout = 10000
--- dist/src/main.js
+++ dist/src/main.js
@@ -1,27 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
-//element-ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-//axios：Ajax请求
-import axios from "./axios.js";
-import hjk from '@/components/index.js'
-//是否为生产环境
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/common/css/reset.css'
+
 Vue.config.productionTip = false
-//use
-Vue.use(ElementUI);
-Vue.use(hjk);
-
-//prototype
-Vue.prototype.$http = axios;
-
-//Vue实例
-window.bus = new Vue()
-
+Vue.use(ElementUI)
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  components: { App },
+  template: '<App/>'
+})
