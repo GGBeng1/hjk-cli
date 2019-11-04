bc76ad7ca1ee3348bed839a10e1859705736deaa
曹梦哲 <15010976823@163.com>
2019-11-04 11:40:52 +0800

    :car: 修复版本问题

--- bin/index.js
+++ bin/index.js
@@ -6,22 +6,22 @@
  */
 
 // 引入依赖
-let program = require('commander')
-let vfs = require('vinyl-fs')
-let through = require('through2')
-const chalk = require('chalk')
-const fs = require('fs-extra')
-const path = require('path')
+let program = require("commander")
+let vfs = require("vinyl-fs")
+let through = require("through2")
+const chalk = require("chalk")
+const fs = require("fs-extra")
+const path = require("path")
 
 // 定义版本号以及命令选项
 program
   .allowUnknownOption()
-  .version('1.2.0')
-  .description('基于vue2.0+ webpack4.0+ 的脚手架')
+  .version("1.3.1")
+  .description("基于vue2.0+ webpack4.0+ 的脚手架")
 
 program
-  .command('init <dir>')
-  .description('创建一个新的  Vue项目骨架')
+  .command("init <dir>")
+  .description("创建一个新的  Vue项目骨架")
   .action(function(dir) {
     // 获取将要构建的项目根目录
     let projectPath = path.resolve(dir)
@@ -34,11 +34,11 @@ program
     fs.ensureDirSync(projectName)
 
     // 获取本地模块下的dist目录
-    let cwd = path.join(__dirname, '../dist')
+    let cwd = path.join(__dirname, "../dist")
 
     // 从dist目录中读取除node_modules目录下的所有文件并筛选处理
     vfs
-      .src(['**/*', '!node_modules/**/*'], { cwd: cwd, dot: true })
+      .src(["**/*", "!node_modules/**/*"], { cwd: cwd, dot: true })
       .pipe(
         through.obj(function(file, enc, callback) {
           if (!file.stat.isFile()) {
@@ -51,44 +51,44 @@ program
       )
       // 将从dist目录下读取的文件流写入到之前创建的文件夹中
       .pipe(vfs.dest(projectPath))
-      .on('end', function() {
-        console.log(chalk.green('Installing packages...'))
+      .on("end", function() {
+        console.log(chalk.green("Installing packages..."))
 
         // 将node工作目录更改成构建的项目根目录下
         process.chdir(projectPath)
 
         // 执行安装命令
-        require('../lib/install')
+        require("../lib/install")
       })
       .resume()
   })
 
 program
-  .command('start')
-  .description('运行开发环境 基于webpack-server')
+  .command("start")
+  .description("运行开发环境 基于webpack-server")
   .action(function name() {
-    require('../lib/dev')
+    require("../lib/dev")
   })
 
 program
-  .command('build')
-  .description('运行生产环境 对项目进行打包')
+  .command("build")
+  .description("运行生产环境 对项目进行打包")
   .action(function name() {
-    require('../lib/build')
+    require("../lib/build")
   })
 
 program.parse(process.argv)
 
-program.on('--help', () => {
-  console.log('  Examples:')
+program.on("--help", () => {
+  console.log("  Examples:")
   console.log()
   console.log(
-    chalk.gray('    # create a new project with an official template')
+    chalk.gray("    # create a new project with an official template")
   )
-  console.log('    $ hjk init my-project')
-  console.log('    $ cd my-project ')
-  console.log('    $ hjk start ')
-  console.log('    $ hjk build ')
+  console.log("    $ hjk init my-project")
+  console.log("    $ cd my-project ")
+  console.log("    $ hjk start ")
+  console.log("    $ hjk build ")
 })
 
 function help() {
